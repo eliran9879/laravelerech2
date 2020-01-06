3509a8f91a345807f36ed4b9742c922b31568165
itamarbo <itamar960@gmail.com>
2020-01-06 18:26:24 +0200

    add logo and sidebar nevigation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>eliranandmoccc</title>
+        <title>Final project 2020</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
