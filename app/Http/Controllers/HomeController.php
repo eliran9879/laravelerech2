788fb1b2da478e9b1ecf9a79e6b5248d3a92a3f2
eliran <eliran9879@gmail.com>
2020-05-01 17:23:35 +0300

    continu with_deshboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,14 +26,36 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $users = User::where(DB::raw("(DATE_FORMAT(created_at,'%Y'))"),date('Y'))
-    				->get();
-        $chart = Charts::database($users, 'bar', 'highcharts')
-			      ->title("Monthly new Register Users")
-			      ->elementLabel("Total Users")
-			      ->dimensions(1000, 500)
-			      ->responsive(false)
-			      ->groupByMonth(date('Y'), true);
-        return view('charts.index',compact('chart'));
+        // $users = User::where(DB::raw("(DATE_FORMAT(created_at,'%Y'))"),date('Y'))
+        //             ->get();
+        // $chart = Charts::database($users, 'bar', 'highcharts')
+		// 	      ->title("Monthly new Register Users")
+		// 	      ->elementLabel("Total Users")
+		// 	      ->dimensions(1000, 500)
+		// 	      ->responsive(false)
+        //           ->groupByMonth(date('Y'), true);
+        $tasks = DB::table('customers') ->get();
+        foreach   ($tasks as $customer) {
+        $customer_occup = $customer->occupation;
+        }
+        $loans = DB::table('banks')
+        ->join('clientdatas', 'banks.id', '=', 'clientdatas.bank_id') 
+         ->where('clientdatas.status', '=' ,'open') ->orWhere('clientdatas.status', '=' ,'close') ->get();
+       $bar_chart = Charts::database( $tasks, 'bar', 'material')
+       ->title('segmentation occuputioan')
+       ->elementLabel("Total Customers")
+       ->Width(0)
+       ->responsive(true)
+       ->Colors(['#4caf50'])
+       ->groupBy('occupation');
+       $bar_chart1 = Charts::database( $loans, 'bar', 'material')
+       ->title('segmentation occuputioan')
+       ->elementLabel("Total transactions by banks")
+       ->Width(0)
+       ->responsive(true)
+       ->Colors(['#4caf50'])
+       ->groupBy('name');
+    
+        return view('charts.index',compact('bar_chart','bar_chart1'));
     }
 }
--- resources/views/charts/index.blade.php
+++ resources/views/charts/index.blade.php
@@ -1,19 +1,30 @@
 @extends('layouts.sidebar')
 
 @section('content')
-<div class="container">
+<div class="container" style= "    background: #a9a9a9a8;
+    border-radius: 2%;">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-8">
             <div class="panel panel-default">
-                <div class="panel-heading">Chart Demo</div>
-
+                <!-- <div class="panel-heading">Chart Demo</div> -->
+                <br>
                 <div class="panel-body">
-                    {!! $chart->html() !!}
+                    
                 </div>
             </div>
         </div>
+
+        <br>
+        </div>
+            <div class="row">
+            <div class="col-md-7"> 
+            <div style="padding-bottom:2%;  "> {!! $bar_chart->html() !!}</div>
+            <div style="padding-bottom:2%;  ">{!! $bar_chart1->html() !!} </div>
+            </div>
+   
     </div>
 </div>
 {!! Charts::scripts() !!}
-{!! $chart->script() !!}
+{!! $bar_chart->script() !!}
+{!! $bar_chart1->script() !!}
 @endsection
