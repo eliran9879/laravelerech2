8fab27c28245d7e650206cc85a42a8da3b3b6e86
eliran <eliran9879@gmail.com>
2020-05-03 10:47:57 +0300

    chang to 6.2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,6 +16,10 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
     /**
      * Show the application dashboard.
      *
