208cb48eee7419eb1e1732c195b377962dbe5ce8
itamarbo <itamar960@gmail.com>
2020-06-13 11:05:47 +0300

    remove import/export in IBI covs

--- resources/views/covenantsibis/index.blade.php
+++ resources/views/covenantsibis/index.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.sidebar')
 @section('content')
 <title>Import Export Excel to database</title>
-  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" /> -->
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" /> -->
 </head>
+
 <body>
-   
+  <!--   
 <div class="container">
     <div class="card bg-light mt-3">
         <div class="card-header">
@@ -21,67 +22,67 @@
         </div>
     </div>
 </div>
-   
+-->
 </body>
-    
-<div class="w3-sidebar w3-bar-block " style="margin: 0 0 5% 0;" >
-         
-         <div class="sidebar-module" style="font-family: Arial Black, Gadget, sans-serif;">
-           <h2>Covanants Ibi</h2>
-           <ol class="list-unstyled">
-            
-           
-             
-          </ol>
-        
-                
- </div>
 
- <div class="panel panel-default">
+<div class="w3-sidebar w3-bar-block " style="margin: 0 0 5% 0;">
 
-<br>
-<div class="table-responsive">
-<table class="table">
-<thead>
-<tr>
-<th >Name</th>
-<th >Designation</th>
-<th >Range</th>
-<th >Max amount</th>
-<th >Approval</th>
-<th >Max general</th>
-<th >Min general</th>
-<th >Action</th>
+  <div class="sidebar-module" style="font-family: Arial Black, Gadget, sans-serif;">
+    <h2>Covanants Ibi</h2>
+    <ol class="list-unstyled">
 
 
-</tr>
-</thead>
 
-<tbody>
-@foreach($covenantsibis as $covenantibi)
-  <tr>
- 
-<td >{{$covenantibi->banks->name}} </td>
-<td > {{$covenantibi->designation}}</td>
-<td > {{$covenantibi->total_month}}</td>
-<td > {{$covenantibi->total_amount}}</td>
-<td > {{$covenantibi->approval}}</td>
-<td > {{$covenantibi->max_percentage_general}}</td>
-<td > {{$covenantibi->min_percentage_general}}</td>
-<td ><a href= "{{route('covenants_ibi.edit', $covenantibi->id )}}"><img src="https://image.flaticon.com/icons/png/512/84/84380.png" style = "width:30px; height:30px; margin-left: auto; margin-right: auto;"> </a></td>
-</tr>
+    </ol>
 
 
-@endforeach
-</tbody>
-</table>
+  </div>
+
+  <div class="panel panel-default">
+
+    <br>
+    <div class="table-responsive">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Designation</th>
+            <th>Range</th>
+            <th>Max amount</th>
+            <th>Approval</th>
+            <th>Max general</th>
+            <th>Min general</th>
+            <th>Action</th>
+
+
+          </tr>
+        </thead>
+
+        <tbody>
+          @foreach($covenantsibis as $covenantibi)
+          <tr>
+
+            <td> {{$covenantibi->banks->name}} </td>
+            <td> {{$covenantibi->designation}}</td>
+            <td> {{$covenantibi->total_month}}</td>
+            <td> {{$covenantibi->total_amount}}</td>
+            <td> {{$covenantibi->approval}}</td>
+            <td> {{$covenantibi->max_percentage_general}}</td>
+            <td> {{$covenantibi->min_percentage_general}}</td>
+            <td><a href="{{route('covenants_ibi.edit', $covenantibi->id )}}"><img src="https://image.flaticon.com/icons/png/512/84/84380.png" style="width:30px; height:30px; margin-left: auto; margin-right: auto;"> </a></td>
+          </tr>
+
+
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+  </div>
+
 </div>
-</div>
 
-</div>
 
- 
- @endsection
+@endsection
 <!-- <footer class="ttt">Website of EISS</footer> 
 
 <style>
